7767fe08363a0bfdf30bddf4860b7b7b126fdddd
Davide Albano <davidealbano96@gmail.com>
2021-04-10 12:50:21 +0200

    basic_setup - started setting up the structure

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,34 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'Zettabit',
+  siteDescription: 'Zettabit, un semplice tech blog',
+  siteUrl: 'https://zettabit.it',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'src/content/**/*.md',
+        typeName: 'Post',
+        refs: {
+          tags: {
+            typeName: 'Tag',
+            create: true
+          },
+        }
+      }
+    },
+  ],
+  templates: {
+    Tag: '/tag/:id',
+    Post: '/content/:path',
+  },
+  transformers: {
+    remark: {
+      autolinkClassName: 'icon icon-link heading-anchor',
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['noopener', ],
+      anchorClassName: 'icon icon-link',
+    }
+  },
 }
