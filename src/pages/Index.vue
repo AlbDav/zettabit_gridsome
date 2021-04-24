aa892a4cc56f909dc8274d8e6d1fc606f24faa5b
Davide Albano <da.albano@reply.it>
2021-04-24 19:17:41 +0200

    basicUI - started styling the home

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,29 +1,53 @@
 <template>
   <Layout>
-    <h1>Hello, world!</h1>
+    <article v-for="post in $page.posts.edges" :key="post.id" class="flex">
+	  <div class="w-2/3 m-1">
+	    <g-image :src="post.node.image" class="object-cover h-full w-full" />
+	  </div>
+	  <div class="w-1/3 m-1">
+        <h2><g-link :to="post.node.path" rel="bookmark">{{ post.node.title }}</g-link></h2>
+        <time :datetime="post.node.date">{{ post.node.date }}</time>
+        <p>{{ post.node.summary }}</p>
+	  </div>
+    </article>
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <h2>Pagination</h2>
+    <Pager :info="$page.posts.pageInfo"/>
   </Layout>
 </template>
 
-<script>
-export default {
-  metaInfo: {
-    title: 'Hello, world!'
+<page-query>
+query Posts ($page: Int) {
+  posts: allPost (sortBy: "date", order: DESC, perPage: 10, page: $page) @paginate {
+    totalCount
+    pageInfo {
+      totalPages
+      currentPage
+    }
+    edges {
+      node {
+        id
+        title
+        date (format: "D MMMM YYYY", locale: "it")
+        summary
+        path
+        content
+		image
+      }
+    }
   }
 }
-</script>
+</page-query>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
+<script>
+import { Pager } from 'gridsome'
+
+export default {
+  components: {
+    Pager
+  },
+  metaInfo: {
+    title: 'View my blog posts'
+  },
 }
-</style>
+</script>
