7aa7a23e6765759993b9d0c2a0da4d8db5f95d87
Davide Albano <da.albano@reply.it>
2021-05-16 11:46:31 +0200

    basicUI - changed auto-fit to auto-fill

--- src/pages/Categories.vue
+++ src/pages/Categories.vue
@@ -1,6 +1,6 @@
 <template>
 	<Layout>
-		<div class="grid grid-cols-auto-fit gap-3">
+		<div class="grid grid-cols-auto-fill gap-3">
 			<div v-for="i in 30" :key="i" class="border h-20 w-20 mx-auto">
 				hello world
 			</div>
