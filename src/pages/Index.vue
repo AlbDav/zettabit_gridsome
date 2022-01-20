4f247a6e20175c81d4932cc38c1090d20f53280d
Davide Albano <davidealbano96@gmail.com>
2022-01-20 18:28:39 +0100

    fixed category and refactored all articles

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,6 @@
 <template>
   <Layout>
-    <main-article :post="topPosts[0]" />
-    <!--     <ClientOnly>
-      <carousel :topPosts="topPosts" :carouselNum="carouselNum" />
-
-      <article-preview v-for="post in otherPosts" :key="post.id" :post="post" />
-    </ClientOnly> -->
-    <article-grid class="w-10/12 sm:px-5 mx-auto my-5" :posts="posts" />
+    <all-articles :posts="posts" />
   </Layout>
 </template>
 
@@ -34,10 +28,7 @@ query Posts ($page: Int) {
 </page-query>
 
 <script>
-import Carousel from "../components/Carousel.vue";
-import ArticlePreview from "../components/ArticlePreview.vue";
-import MainArticle from "../components/MainArticle.vue";
-import ArticleGrid from "../components/ArticleGrid.vue";
+import AllArticles from "../components/AllArticles.vue";
 
 export default {
   computed: {
@@ -58,32 +49,9 @@ export default {
         return [];
       }
     },
-    multiplePosts() {
-      let posts = [];
-      for (let i = 0; i < 4; i++) {
-        posts.push(this.posts);
-      }
-      return posts.flat();
-    },
-    carouselNum() {
-      return this.$screen.sm ? 5 : 3;
-    },
-    topPosts() {
-      let sliceNum =
-        this.multiplePosts.length > this.carouselNum ? this.carouselNum : 1;
-      return this.multiplePosts.slice(0, sliceNum);
-    },
-    otherPosts() {
-      let sliceNum =
-        this.multiplePosts.length > this.carouselNum ? this.carouselNum : 1;
-      return this.multiplePosts.slice(sliceNum);
-    },
   },
   components: {
-    Carousel,
-    ArticlePreview,
-    MainArticle,
-    ArticleGrid,
+    AllArticles,
   },
   metaInfo: {
     title: "View my blog posts",
