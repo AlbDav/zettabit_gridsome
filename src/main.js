626187d930b665e688db1c3d11a801e632e873d1
Davide Albano <davidealbano96@gmail.com>
2022-01-26 11:45:46 +0100

    fxied twitter widget and mythic quest date

--- src/main.js
+++ src/main.js
@@ -6,13 +6,6 @@ import './index.css';
 import VueScreen from 'vue-screen';
 
 export default function (Vue, { router, head, isClient }) {
-	head.script.push({
-    id: 'twitter-wjs',
-    type: 'text/javascript',
-    async: true,
-    defer: true,
-    src: 'https://platform.twitter.com/widgets.js'
-  })
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout);
   Vue.use(VueScreen, 'tailwind');
