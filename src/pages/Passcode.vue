9ff45b77a879fbb3f4048419a4ff032d459c17e3
Davide Albano <davidealbano96@gmail.com>
2022-05-13 13:48:39 +0200

    almost finished passcode

--- src/pages/Passcode.vue
+++ src/pages/Passcode.vue
@@ -1,12 +1,17 @@
 <template>
-    <div class="h-screen flex justify-center items-center bg-gray-400">
-        <div class="w-full md:w-1/3">
-            <div class="grid grid-cols-5 gap-4 mb-5">
-                <input type="text" v-for="i in 5" :ref="getInputRef(i)" class="p-3 rounded-lg shadow-inner border outline-none" readonly @click="setFocus(i)">
+    <div ref="backGroundContainer" class="h-screen flex justify-center items-center bg-gray-400">
+        <div class="w-full md:w-1/3 mx-3">
+            <div ref="digitsContainer" class="grid grid-cols-5 gap-4 mb-5">
+                <input type="text" v-for="digit in digits"
+                    :key="digit.id"
+                    :ref="getInputRef(digit.id)"
+                    v-model="digit.value"
+                    class="p-3 rounded-lg shadow-inner border outline-none text-center"
+                    readonly @click="setFocus(digit.id)">
             </div>
             <div class="grid grid-cols-3 gap-4 items-center">
                 <button v-for="button in buttons" :key="button.id" @click="button.click" class="p-3 button-shadow rounded-lg bg-gray-900 text-white">
-                    {{ button.text }}
+                    {{ button.value }}
                 </button>
             </div>
         </div>
@@ -17,79 +22,71 @@
 export default {
     data: () => {
         return {
-            focusedInput: undefined
+            focusedInput: undefined,
+            focusedInputIndex: undefined,
+            digits: [...Array(5).keys()].map(el => {
+                    return {
+                        id: el,
+                        value: ''
+                    }
+                })
         }
     },
     computed: {
             buttons() {
-                return [
+                let numberButtons = [...Array(9).keys()].map(el => {
+                    let num = el + 1
+                    num = num.toString()
+                    return {
+                        id: num,
+                        value: num,
+                        click: (e) => this.addNumber(e, num)
+                    }
+                });
+                return [...numberButtons,
                 {
-                    id: '1',
-                    text: '1',
-                    click: () => this.addNumber('1')
-                },
-                {
-                    id: '2',
-                    text: '2',
-                    click: () => this.addNumber('2')
-                },
-                {
-                    id: '3',
-                    text: '3',
-                    click: () => this.addNumber('3')
-                },
-                {
-                    id: '4',
-                    text: '4',
-                    click: () => this.addNumber('4')
-                },
-                {
-                    id: '5',
-                    text: '5',
-                    click: () => this.addNumber('5')
-                },
-                {
-                    id: '6',
-                    text: '6',
-                    click: () => this.addNumber('6')
-                },
-                {
-                    id: '7',
-                    text: '7',
-                    click: () => this.addNumber('7')
-                },
-                {
-                    id: '8',
-                    text: '8',
-                    click: () => this.addNumber('8')
-                },
-                {
-                    id: '9',
-                    text: '9',
-                    click: () => this.addNumber('9')
+                    id: 'C',
+                    value: 'C',
+                    click: (e) => this.removeNumber(e)
                 },
                 {
                     id: '0',
-                    text: '0',
-                    click: () => this.addNumber('0')
-                },
-                {
-                    id: 'C',
-                    text: 'C',
-                    click: () => console.log('ciao')
+                    value: '0',
+                    click: (e) => this.addNumber(e, '0')
                 },
                 {
                     id: 'GO',
-                    text: 'GO',
-                    click: () => console.log('ciao')
+                    value: 'GO',
+                    click: (e) => this.checkCode(e)
                 },
             ]
             }
     },
     methods: {
-        addNumber(num) {
-            if (this.focusedInput) {
-                this.focusedInput.value = num;
+        addNumber(e, num) {
+            this.clickEffect(e.target);
+            if (this.focusedInputIndex !== undefined) {
+                let digit = JSON.parse(JSON.stringify(this.digits[this.focusedInputIndex]));
+                digit.value = num;
+                console.log(digit);
+                this.$set(this.digits, this.focusedInputIndex, digit);
+            }
+        },
+        removeNumber(e) {
+            this.clickEffect(e.target);
+            if (this.focusedInputIndex !== undefined) {
+                let digit = JSON.parse(JSON.stringify(this.digits[this.focusedInputIndex]));
+                digit.value = ''
+                this.$set(this.digits, this.focusedInputIndex, digit);
+            }
+        },
+        checkCode(e) {
+            this.clickEffect(e.target);
+            let code = this.digits.reduce((prev, curr) => prev + curr.value, '');
+            if (code !== '71926') {
+                this.wrongCode();
+            } else {
+                this.rightCode();
             }
         },
         getInputRef(i) {
@@ -102,7 +99,28 @@ export default {
             let ref = this.getInputRef(i);
             let el = this.$refs[ref];
             this.focusedInput = el[0];
+            this.focusedInputIndex = i;
             this.focusedInput.classList.add('focused-input');
+        },
+        clickEffect(button) {
+            button.classList.add('button-shadow-active');
+            setTimeout(() => {
+                button.classList.remove('button-shadow-active')
+            }, 100);
+        },
+        wrongCode() {
+            let container = this.$refs.digitsContainer;
+            let backGround = this.$refs.backGroundContainer;
+            container.classList.add('wrong-code');
+            backGround.classList.add('wrong-bg');
+            setTimeout(() => {
+                container.classList.remove('wrong-code');
+                backGround.classList.remove('wrong-bg');
+            }, 1000);
+        },
+        rightCode() {
+            let backGround = this.$refs.backGroundContainer;
+            backGround.classList.add('right-bg');
         }
     }
 }
@@ -113,11 +131,41 @@ export default {
 	box-shadow: 0px 8px 0px 0px theme('colors.gray.600');
 	transition: all 0.1s ease-in-out;
 }
-.button-shadow:active {
-	box-shadow: none;
+.button-shadow-active {
+	box-shadow: none !important;
 	transform: translateY(10px);
 }
 .focused-input {
     background-color: theme('colors.yellow.100');
+}
+.right-bg {
+    background-color: theme('colors.green.500') !important;
+}
+.wrong-bg {
+    background-color: theme('colors.red.500') !important;
+}
+.wrong-code {
+  animation: shake 0.82s cubic-bezier(.36,.07,.19,.97) both;
+  transform: translate3d(0, 0, 0);
+  backface-visibility: hidden;
+  perspective: 1000px;
+}
+
+@keyframes shake {
+  10%, 90% {
+    transform: translate3d(-1px, 0, 0);
+  }
+  
+  20%, 80% {
+    transform: translate3d(2px, 0, 0);
+  }
+
+  30%, 50%, 70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+
+  40%, 60% {
+    transform: translate3d(4px, 0, 0);
+  }
 }
 </style>
